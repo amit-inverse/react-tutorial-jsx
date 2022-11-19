d897a22d0ec5f8d2654b6f4b7f9495942d5dd711
Amit Kumar Das <amit.inverse@gmail.com>
2022-11-19 20:29:59 +0530

    Extracting Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // 1. Import the React and ReactDOM libraries
-import { buildQueries } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2. Get a reference to the div with ID root
 const el = document.getElementById('root');
@@ -10,21 +10,6 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
-function App() {
-    // 1. All prop name follow camelCase
-    // return <textarea autoFocus={true} />;
-
-    // 2. Number attribute use curly braces
-    // return <input maxLength={5} />;
-
-    // 3. Boolean 'true' can be written with just the property name. 'false' should be written with curly braces
-    // return <input spellCheck />;
-
-    // 4. The 'class' attribute is written as 'className'
-
-    // 5. In-line styles are provided as objects
-    return <input style={{ border: '1px solid blue' }} />;
-}
 
 // 5. Show the component on the screen
 root.render(<App />);
